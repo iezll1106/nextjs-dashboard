2e612a222890146f7ecf5c21889d55bbdaca8896
iezll1106 <llizardo@byupathway.edu>
2025-03-26 10:50:05 +0800

    edit edit-form.tsx

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useState } from 'react';
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
   CheckIcon,
@@ -19,26 +18,10 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
 
-  async function handleSubmit(formData: FormData) {
-    setErrorMessage(null); // Clear previous errors
-
-    const result = await updateInvoice(invoice.id, {}, formData);
-
-    if (result?.errors) {
-      setErrorMessage(result.message); // Display validation errors
-    }
-  }
-
-  return (
-    <form action={handleSubmit}>
+  return <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
-        {/* Error Message Display */}
-        {errorMessage && (
-          <p className="mb-4 text-red-500 text-sm">{errorMessage}</p>
-        )}
-
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
@@ -70,16 +53,18 @@ export default function EditInvoiceForm({
             Choose an amount
           </label>
           <div className="relative mt-2 rounded-md">
-            <input
-              id="amount"
-              name="amount"
-              type="number"
-              step="0.01"
-              defaultValue={invoice.amount}
-              placeholder="Enter USD amount"
-              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-            />
-            <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            <div className="relative">
+              <input
+                id="amount"
+                name="amount"
+                type="number"
+                step="0.01"
+                defaultValue={invoice.amount}
+                placeholder="Enter USD amount"
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              />
+              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
           </div>
         </div>
 
@@ -126,7 +111,6 @@ export default function EditInvoiceForm({
           </div>
         </fieldset>
       </div>
-
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/invoices"
